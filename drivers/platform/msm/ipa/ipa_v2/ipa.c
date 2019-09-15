72075470317563e425a60604a12d790352bebbeb
xNombre <kartapolska@gmail.com>
2019-09-16 00:14:43 +0200

    drivers: msm: ipa_v2: don't hold a wakelock
    IPA_WL is keeping device awake for long time.
    As of my tests there are no actual problems after disabling it.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -3815,6 +3815,7 @@ void ipa2_dec_client_disable_clks(struct ipa_active_client_logging_info *id)
 * Return codes:
 * None
 */
+#if 0
 void ipa_inc_acquire_wakelock(enum ipa_wakelock_ref_client ref_client)
 {
 	unsigned long flags;
@@ -3861,6 +3862,12 @@ void ipa_dec_release_wakelock(enum ipa_wakelock_ref_client ref_client)
 	}
 	spin_unlock_irqrestore(&ipa_ctx->wakelock_ref_cnt.spinlock, flags);
 }
+#else
+void ipa_inc_acquire_wakelock(enum ipa_wakelock_ref_client ref_client)
+{ }
+void ipa_dec_release_wakelock(enum ipa_wakelock_ref_client ref_client)
+{ }
+#endif
 
 static int ipa_setup_bam_cfg(const struct ipa_plat_drv_res *res)
 {
